c6214286db7cc31deb4c608312f75f782270b939
tanglie1993 <tanglie23@163.com>
2015-01-15 17:42:31 +0800

    initial commit
    包含两个activity，能把hot的前几条内容显示在一个listView里面，并在点击后启动一个新的Activity显示图片。

---
+++ My9GAG/app/src/main/java/com/example/tanglie1993/my9gag/Feed.java
@@ -0,0 +1,45 @@
+package com.example.tanglie1993.my9gag;
+
+import android.database.Cursor;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+/**
+ * Created by storm on 14-3-25.
+ */
+public class Feed {
+    private static final HashMap<String, Feed> CACHE = new HashMap<String, Feed>();
+
+    public String id;
+    public String caption;
+    public String link;
+    public Image images;
+    public Vote votes;
+
+    public class Image {
+        public String small;
+        public String normal;
+        public String large;
+    }
+
+    private class Vote {
+        public int count;
+    }
+
+    public static class FeedRequestData {
+        public ArrayList<Feed> data;
+        public Paging paging;
+
+        public String getPage() {
+            return paging.next;
+        }
+    }
+
+    private class Paging {
+        public String next;
+    }
+}
---
+++ My9GAG/app/src/main/java/com/example/tanglie1993/my9gag/ImageActivity.java
@@ -0,0 +1,66 @@
+package com.example.tanglie1993.my9gag;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
+
+
+public class ImageActivity extends ActionBarActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+        requestData();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_image, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void requestData(){
+        imageView= (ImageView) findViewById(R.id.imageView);
+        Bundle bundle = getIntent().getExtras();
+        RequestQueue newRequestQueue = Volley.newRequestQueue(ImageActivity.this);
+        ImageRequest imageRequest = new ImageRequest((String) bundle.get("imageurl"),
+                new Response.Listener<Bitmap>()
+                {
+                    @Override
+                    public void onResponse(Bitmap response)
+                    {
+                        imageView.setImageBitmap(response);
+                    }
+                }, 0, 0, Bitmap.Config.RGB_565, null);
+        newRequestQueue.add(imageRequest);
+
+    }
+}
